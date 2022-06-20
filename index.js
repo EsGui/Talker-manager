837d29826a030e3d3c68d0fc770aa0b1195e8a93
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2022-06-20 17:03:10 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +8,20 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const nomeDoArquivo = 'talker.json';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_request, response) => {
+  const data = fs.readFileSync(nomeDoArquivo, 'utf-8');
+  if (data.length > 0) {
+    response.status(200).json(JSON.parse(data));
+  } else {
+    response.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {
