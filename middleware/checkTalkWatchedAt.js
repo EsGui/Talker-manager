585badc22bbd40e58db58a062098a37887bed52d
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2022-06-21 09:09:30 -0300

    Referenciando regex

--- middleware/checkTalkWatchedAt.js
+++ middleware/checkTalkWatchedAt.js
@@ -1,5 +1,6 @@
 function checkTalkWatchedAt(req, res, next) {
     const { talk } = req.body;
+    // O Regex abaixo de validação de data eu peguei no course.
     const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
     if (!talk.watchedAt) {
         return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
